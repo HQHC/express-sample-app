ceb291b6fd08653a8c830e1b1707026ea10846aa
清魄 <meng.mfb@alibaba-inc.com>
2023-07-03 12:02:28 +0800

    fix: 修改bug

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const request = require('request');
+import express from "express";
+import request from "request";
 
 const app = express();
 
-app.use('/', (req, res) => {
+app.use("/", (req, res) => {
   const url = `https://api.openai.com${req.url}`;
   req.pipe(request(url)).pipe(res);
 });
