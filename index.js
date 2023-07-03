96f73018842d16a41a2ec125fcb8f8d3fd313e3c
清魄 <meng.mfb@alibaba-inc.com>
2023-07-03 13:48:56 +0800

    fix: 修改为axios

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 import express from "express";
-import request from "request";
+import axios from 'axios';
 
 const app = express();
 
-app.use("/", (req, res) => {
-  const url = `https://api.openai.com${req.url}`;
-  console.log(url)
-  req.pipe(request(url)).pipe(res);
-});
+app.use(express.json());
+
+app.all('/api/*', async (req, res) => {
+    try {
+      const { method, originalUrl, body, query } = req;
+
+      console.log(process.env.OPENAI_API_KEY)
+  
+      const response = await axios({
+        method,
+        url: `https://api.openai.com${originalUrl}`,
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        params: query,
+        data: body,
+      });
+  
+      res.json(response.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Proxy server listening on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
